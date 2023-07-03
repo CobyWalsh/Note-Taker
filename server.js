cea099e5c5560e103fc58b0ad5795049cd771bbe
coby walsh <cobywalsh7@yahoo.com>
2023-07-03 09:06:49 -0600

    made corrections to routes.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
-const app = require("./routes/routes");
+// const app = require("./routes/routes");
 const apiRoutes = require("./routes/routes")
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'))
-app.use('/api', apiRoutes);
+app.use('/', apiRoutes);
 app.listen(PORT, function() {
     console.log("app listening on PORT: " + PORT);
 });  

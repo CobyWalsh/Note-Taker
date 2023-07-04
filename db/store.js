5e2b2abe4e1640c73876036172dd7f5119211a5a
coby walsh <cobywalsh7@yahoo.com>
2023-07-04 08:05:25 -0600

    still trying to get the notes to save on the webpage

--- db/store.js
+++ db/store.js
@@ -2,7 +2,7 @@ const util = require('util');
 const fs = require('fs');
 
 // This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
-const uuidv1 = require('uuid/v1');
+// const uuidv1 = require('uuid/v1');
 
 const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
@@ -17,20 +17,24 @@ class Store {
     }
   
     getNotes() {
-      return this.read().then(notes) => {
+      return this.read().then((notes) => {
         var notes = JSON.parse(notes);
         console.log(notes)
-        res.json(notes);
+        res.json(uuidv1,"");
       });
     }
   
     addNote(note) {
-    
+        var notes = JSON.parse(data);
+        console.log(notes)
+        res.json(uuidv1, "");
     }
   
     removeNote(id) {
       // Get all notes, remove the note with the given id, write the filtered notes
-   
+      var notes = JSON.filter(data);
+      console.log(notes)
+      res.json(uuidv1);
     }
   }
   

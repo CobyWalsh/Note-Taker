823dedea61f01c70d66f686ce9b80a5e4271605c
coby walsh <cobywalsh7@yahoo.com>
2023-07-04 10:21:53 -0600

    trying to make my notes show up on my browser

--- server.js
+++ server.js
@@ -2,10 +2,11 @@
 const express = require("express");
 const fs = require("fs");
 const path = require('path');
-const apiRoutes = require("./routes/routes")
+const apiRoutes = require("./routes/routes");
 const app = express();
 const PORT = process.env.PORT || 3000;
-const htmlRoutes = require('./routes/htmlRoutes')
+const htmlRoutes = require('./routes/htmlRoutes');
+let filename =  require('db.json');
 
 // uses express to send data to specific file using the const used above
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +14,8 @@ app.use(express.json());
 app.use(express.static('public'))
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
+fs.writeFileSync(filename, JSON.stringify(content));
 
 // makes sure the port is listening to our 3000 port
 app.listen(PORT, function() {
